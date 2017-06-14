dbede7a5778af260ee2ed5186bcf2a47d9379942
hsbaewa <hsbaewa@gmail.com>
2017-06-14 11:42:00 +0900

    핀치줌 이미지뷰 추가 github : https://github.com/chrisbanes/PhotoView

---
+++ lib/src/main/java/kr/co/hs/view/pinchzoomimageview/PinchZoomImageView.java
@@ -0,0 +1,24 @@
+package kr.co.hs.view.pinchzoomimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * 생성된 시간 2017-06-14, Bae 에 의해 생성됨
+ * 프로젝트 이름 : HsCommon
+ * 패키지명 : kr.co.hs.view.pinchzoomimageview
+ */
+
+public class PinchZoomImageView extends PhotoView {
+    public PinchZoomImageView(Context context) {
+        super(context);
+    }
+
+    public PinchZoomImageView(Context context, AttributeSet attr) {
+        super(context, attr);
+    }
+
+    public PinchZoomImageView(Context context, AttributeSet attr, int defStyle) {
+        super(context, attr, defStyle);
+    }
+}
